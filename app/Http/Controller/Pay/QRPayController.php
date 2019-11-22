5d15f22a9429ad4b9955a0cc86a17acdf2a8ea2c
mike <635105058@qq.com>
2019-11-22 16:24:42 +0800

    test

--- app/Http/Controller/Pay/QRPayController.php
+++ app/Http/Controller/Pay/QRPayController.php
@@ -38,7 +38,7 @@ class QRPayController
         $wapInstance = new QRPay(config("pay.merchant_no"));
         $redirectResult = $wapInstance->payOrder($terminalInfo["terminal_id"], (int)($totalFee*100),$terminalInfo["access_token"]);
         if(empty($redirectResult))return context()->getResponse()->withData(["status"=>false,"errcode"=>1,"data"=>"Failed"]);
-        return context()->getResponse()->withCookie("trace_id",$redirectResult["trace_id"])->withData(["status"=>true,"errcode"=>0,"data"=>$redirectResult["redirect_url"]]);
+        return context()->getResponse()->withCookie("trace_id",["value"=>$redirectResult["trace_id"],"path"=>"/"])->withData(["status"=>true,"errcode"=>0,"data"=>$redirectResult["redirect_url"]]);
     }
 
     /**
