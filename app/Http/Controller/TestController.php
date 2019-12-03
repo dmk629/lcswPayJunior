80a1a6bed453bb458ba1960e671146f3c24208aa
mike <635105058@qq.com>
2019-12-03 11:08:27 +0800

    test

---
+++ app/Http/Controller/TestController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Http\Controller;
+
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
+use Swoft\Http\Message\Request;
+use App\Exception\ApiException;
+use Swoft\Http\Server\Annotation\Mapping\Middleware;
+use App\Http\Middleware\ControllerMiddleware;
+use Swoft\Validator\Annotation\Mapping\Validate;
+use Swoft\Bean\BeanFactory;
+use App\Lib\Pay\Barcode;
+use Throwable;
+
+/**
+ * TestController
+ * @Controller(prefix="/test")
+ */
+class TestController
+{
+
+    /**
+     * test
+     * @RequestMapping(route="/test",method=RequestMethod::POST)
+     * @Middleware(ControllerMiddleware::class)
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     * @throws Throwable
+     */
+    public function payForBarcode(Request $request)
+    {
+        $post = $request->post();
+        return formatResponse(true,0,$post);
+    }
+
+}
