066b5d5a1bd2b23dde327ef02e7b6998fef5fe90
DongMike <635105058@qq.com>
2019-12-15 21:22:37 +0800

    dream_net test

---
+++ app/Http/Controller/DreamController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Http\Controller;
+
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
+use Swoft\Http\Message\Request;
+use App\Exception\ApiException;
+use Swoft\Http\Server\Annotation\Mapping\Middleware;
+use App\Http\Middleware\ControllerMiddleware;
+use Swoft\Validator\Annotation\Mapping\Validate;
+use Swoft\Bean\BeanFactory;
+use Throwable;
+
+/**
+ * DreamController
+ * @Controller(prefix="/api")
+ */
+class DreamController
+{
+
+    /**
+     * test
+     * @RequestMapping(route="dream",method={RequestMethod::POST,RequestMethod::OPTIONS})
+     * @Middleware(ControllerMiddleware::class)
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     * @throws Throwable
+     */
+    public function notifyTest(Request $request)
+    {
+        $cityJson = file_get_contents("./city.json");
+        $cityInfo = json_decode($cityJson,true);
+        $returnArray = [];
+        foreach($cityInfo as $key=>$value){
+            //var_dump($value);
+            $returnArray[$key]["name"] = $value["name"];
+            foreach($value["cityList"] as $item){
+                $returnArray[$key]["city"][] = $item["name"];
+            }
+        }
+        var_dump($returnArray);
+
+        return context()->getResponse()->withData(["return_code"=>"01","return_msg"=>"success"]);
+    }
+
+}
