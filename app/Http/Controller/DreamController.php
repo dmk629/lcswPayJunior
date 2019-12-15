77b5b50c34d7df5991f90bf17b8c54bf9ad88ad4
DongMike <635105058@qq.com>
2019-12-15 23:32:54 +0800

    dream_net test

--- app/Http/Controller/DreamController.php
+++ app/Http/Controller/DreamController.php
@@ -9,7 +9,8 @@ use App\Exception\ApiException;
 use Swoft\Http\Server\Annotation\Mapping\Middleware;
 use App\Http\Middleware\ControllerMiddleware;
 use Swoft\Validator\Annotation\Mapping\Validate;
-use Swoft\Bean\BeanFactory;
+use App\Model\Entity\DnArea;
+use Swoft\Db\DB;
 use Throwable;
 
 /**
@@ -29,21 +30,42 @@ class DreamController
      * @return mixed
      * @throws Throwable
      */
-    public function notifyTest(Request $request)
+    public function test(Request $request)
     {
         $cityJson = file_get_contents(__DIR__."/city.json");
         $cityInfo = json_decode($cityJson,true);
 
         $returnArray = [];
+        DB::beginTransaction();
         foreach($cityInfo as $key=>$value){
-            //var_dump($value);
-            $returnArray[$key]["name"] = $value["name"];
+            $provinceId = $this->add([
+                "pid" => 0,
+                "name" => $value["name"],
+                "level" => 1
+            ]);
             foreach($value["cityList"] as $item){
-                $returnArray[$key]["city"][] = $item["name"];
+                $cityId = $this->add([
+                    "pid" => $provinceId,
+                    "name" => $item["name"],
+                    "level" => 2
+                ]);
+                if(empty($cityId)){
+                    DB::rollBack();
+                    break 2;
+                }
             }
         }
         var_dump($returnArray);
+        DB::commit();
         return context()->getResponse()->withData(["return_code"=>"01","return_msg"=>"success"]);
+    }
+
+    public function add($cityInfo){
+        return DnArea::insertGetId([
+            "ar_pid" => $cityInfo["pid"],
+            "ar_name" => $cityInfo["name"],
+            "ar_level" => $cityInfo["level"]
+        ]);
     }
 
 }
